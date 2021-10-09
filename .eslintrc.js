7bedc7e6ae5672c3282e6a2fa2ec2c92ed7d0205
刘泽锦 <liuzejin@corp.netease.com>
2021-10-09 18:02:25 +0800

    c

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [require.resolve('@lzj-cli-dev/fabric/lib/eslint')],
+  globals: {
+    page: true,
+  },
+
+  rules: {
+    // your rules
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import objectC from './template/z.js';
+
+function App() {
+  const T = [];
+  const [content, setContent] = React.useState('0.Async');
+
+  for (const key in objectC) {
+    T.push(<a className={content === key ? 'active' : ''} key={`oc_${key}`} onClick={() => setContent(key)}>{key}</a>);
+  }
+
+  return (
+    <div className="App">
+      <content>
+        <div className="nav-box">
+        笔记
+          <div className="nav">
+            {
+              T
+            }
+          </div>
+        </div>
+        <div className="html-box">
+          <div className="html" dangerouslySetInnerHTML={{ __html: objectC[content] }} />
+        </div>
+      </content>
+    </div>
+  );
+}
+
+export default App;
